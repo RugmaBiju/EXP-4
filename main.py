c3309e36ea1640349d08f98aa7f0e5fe06269795
RugmaBiju <rugmabiju@gmail.com>
2026-02-10 19:33:47 +0530

    Integrated all features into main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # main.py
 print("Calculator App")
+
+from add import add
+from sub import sub
+from mul import mul
+
+a = 10
+b = 5
+
+print("Add:", add(a, b))
+print("Sub:", sub(a, b))
+print("Mul:", mul(a, b))
